50b524b59ac7b57d59d3783d960b77a14942570f
Damian Ambrozkiewicz <d.ambrozkiewicz@livechatinc.com>
2021-04-16 10:22:15 +0200

    Add agent rtm/web examples

---
+++ examples/agent_rtm_example.py
@@ -0,0 +1,20 @@
+''' Agent RTM client example usage. '''
+
+from livechat.agent.rtm.client import AgentRTM
+
+agent_rtm = AgentRTM.get_client()
+agent_rtm.open_connection()
+agent_rtm.login(token='Bearer <your bearer token>')
+results = agent_rtm.start_chat(continuous=True)
+chat_id = results['response']['payload']['chat_id']
+thread_id = results['response']['payload']['thread_id']
+agent_rtm.send_event(chat_id=chat_id,
+                     event={
+                         'type': 'message',
+                         'text': 'Hello from Agent RTM!',
+                         'recipients': 'all'
+                     })
+agent_rtm.get_chat(chat_id=chat_id, thread_id=thread_id)
+agent_rtm.deactivate_chat(id=chat_id)
+agent_rtm.logout()
+agent_rtm.close_connection()
---
+++ examples/agent_web_example.py
@@ -0,0 +1,16 @@
+''' Agent WEB client example usage. '''
+
+from livechat.agent.web.client import AgentWeb
+
+agent_web = AgentWeb.get_client(access_token='Bearer <your bearer token>')
+results = agent_web.start_chat(continuous=True)
+chat_id = results['response']['payload']['chat_id']
+thread_id = results['response']['payload']['thread_id']
+agent_web.send_event(chat_id=chat_id,
+                     event={
+                         'type': 'message',
+                         'text': 'Hello from Agent WEB!',
+                         'recipients': 'all'
+                     })
+agent_web.get_chat(chat_id=chat_id, thread_id=thread_id)
+agent_web.deactivate_chat(id=chat_id)
