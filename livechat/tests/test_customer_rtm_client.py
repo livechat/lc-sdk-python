5aaa9d52fbddc32c4176f585febdf4832a83327f
skamieniarz <s.kamieniarz@livechat.com>
2022-01-19 13:20:11 +0100

    API-9951: Update tests according to versions switch

--- livechat/tests/test_customer_rtm_client.py
+++ livechat/tests/test_customer_rtm_client.py
@@ -2,14 +2,18 @@
 
 # pylint: disable=E1120,W0621,C0103
 
+from configparser import ConfigParser
+
 import pytest
 
 from livechat.customer.rtm.client import CustomerRTM
 
-LICENSE_ID = 10386012
+config = ConfigParser()
+config.read('configs/main.ini')
+stable_version = config.get('api_versions', 'stable')
+dev_version = config.get('api_versions', 'dev')
+
 ORGANIZATION_ID = '30007dab-4c18-4169-978d-02f776e476a5'
-STABLE_VERSION = '3.3'
-DEV_PREVIEW_VERSION = '3.4'
 
 
 def test_get_client_without_args():
@@ -18,33 +22,13 @@ def test_get_client_without_args():
         CustomerRTM.get_client()
     assert str(
         exception.value
-    ) == 'Pipe was not opened. Please check your `license_id` argument.'
-
-
-# TODO replace `test_get_client_without_args` with this when v3.4 becomes stable version.
-def test_get_client_without_organization_id():
-    ''' Test if ValueError raised without `organization_id` arg. '''
-    with pytest.raises(ValueError) as exception:
-        CustomerRTM.get_client(version='3.4')
-    assert str(
-        exception.value
     ) == 'Pipe was not opened. Please check your `organization_id` argument.'
 
 
-def test_get_client_with_incorrect_license_id_type():
-    ''' Test if ValueError raised with incorrect `license_id` type. '''
-    with pytest.raises(ValueError) as exception:
-        CustomerRTM.get_client(license_id='Not integer')
-    assert str(
-        exception.value
-    ) == 'Pipe was not opened. Please check your `license_id` argument.'
-
-
-# TODO replace `test_get_client_with_incorrect_license_id_type` with this when v3.4 becomes stable version.
 def test_get_client_with_incorrect_organization_id_type():
     ''' Test if ValueError raised with incorrect `organization_id` type. '''
     with pytest.raises(ValueError) as exception:
-        CustomerRTM.get_client(version='3.4', organization_id=420)
+        CustomerRTM.get_client(organization_id=420)
     assert str(
         exception.value
     ) == 'Pipe was not opened. Please check your `organization_id` argument.'
@@ -53,41 +37,26 @@ def test_get_client_with_incorrect_organization_id_type():
 def test_get_client_with_non_existing_version():
     ''' Test if ValueError raised for non-existing version. '''
     with pytest.raises(ValueError) as exception:
-        CustomerRTM.get_client(license_id=LICENSE_ID, version='2.9')
+        CustomerRTM.get_client(organization_id=ORGANIZATION_ID, version='2.9')
     assert str(exception.value) == 'Provided version does not exist.'
 
 
 def test_get_client():
     ''' Test if created client opens and closes socket in default url. '''
-    client = CustomerRTM.get_client(license_id=LICENSE_ID)
+    client = CustomerRTM.get_client(organization_id=ORGANIZATION_ID)
     client.open_connection()
     opened_state = client.ws.keep_running
     client_url = client.ws.url
     client.close_connection()
     closed_state = client.ws.keep_running
-    assert client_url == f'wss://api.livechatinc.com/v{STABLE_VERSION}/customer/rtm/ws?license_id={LICENSE_ID}', 'Incorrect WS address.'
-    assert opened_state is True, 'Client did not open socket.'
-    assert closed_state is False, 'Client did not close socket.'
-
-
-# TODO replace `test_get_client` with this when v3.4 becomes stable version.
-def test_get_client_dev_preview():
-    ''' Test if created client opens and closes socket in dev-preview version. '''
-    client = CustomerRTM.get_client(version=DEV_PREVIEW_VERSION,
-                                    organization_id=ORGANIZATION_ID)
-    client.open_connection()
-    opened_state = client.ws.keep_running
-    client_url = client.ws.url
-    client.close_connection()
-    closed_state = client.ws.keep_running
-    assert client_url == f'wss://api.livechatinc.com/v{DEV_PREVIEW_VERSION}/customer/rtm/ws?organization_id={ORGANIZATION_ID}', 'Incorrect WS address.'
+    assert client_url == f'wss://api.livechatinc.com/v{stable_version}/customer/rtm/ws?organization_id={ORGANIZATION_ID}', 'Incorrect WS address.'
     assert opened_state is True, 'Client did not open socket.'
     assert closed_state is False, 'Client did not close socket.'
 
 
 def test_client_logs_in_with_token():
     ''' Test if created client can send request. '''
-    client = CustomerRTM.get_client(license_id=LICENSE_ID)
+    client = CustomerRTM.get_client(organization_id=ORGANIZATION_ID)
     client.open_connection()
     response = client.login(token='Bearer 10386012')
     client.close_connection()
@@ -101,7 +70,7 @@ def test_client_logs_in_with_token():
 
 def test_client_logs_in_with_payload():
     ''' Test if created client can send request. '''
-    client = CustomerRTM.get_client(license_id=LICENSE_ID)
+    client = CustomerRTM.get_client(organization_id=ORGANIZATION_ID)
     client.open_connection()
     response = client.login(
         payload={
--- livechat/tests/test_ws_client.py
+++ livechat/tests/test_ws_client.py
@@ -2,25 +2,34 @@
 
 # pylint: disable=E1120,W0621,C0103,R1702
 
+from configparser import ConfigParser
+
 import pytest
 import websocket
 
 from livechat.utils.ws_client import WebsocketClient
 
+config = ConfigParser()
+config.read('configs/main.ini')
+stable_version = config.get('api_versions', 'stable')
+query_string = 'organization_id=30007dab-4c18-4169-978d-02f776e476a5'
+
 
 def test_websocket_client():
     ''' Test if ws can be created and has correct url. '''
     ws = WebsocketClient(
-        url='wss://api.livechatinc.com/v3.3/customer/rtm/ws?license_id=10386012'
+        url=
+        f'wss://api.livechatinc.com/v{stable_version}/customer/rtm/ws?{query_string}'
     )
     assert ws is not None, 'Websocket object was not created.'
-    assert ws.url == 'wss://api.livechatinc.com/v3.3/customer/rtm/ws?license_id=10386012', 'Incorrect WS address.'
+    assert ws.url == f'wss://api.livechatinc.com/v{stable_version}/customer/rtm/ws?{query_string}', 'Incorrect WS address.'
 
 
 def test_websocket_connections_states():
     ''' Test if ws connection states open and close are set correctly. '''
     ws = WebsocketClient(
-        url='wss://api.livechatinc.com/v3.3/customer/rtm/ws?license_id=10386012'
+        url=
+        f'wss://api.livechatinc.com/v{stable_version}/customer/rtm/ws?{query_string}'
     )
     ws.open()
     opened_state = ws.keep_running
@@ -36,7 +45,7 @@ def test_websocket_send_through_not_opened_pipe():
                        ) as exception:
         ws = WebsocketClient(
             url=
-            'wss://api.livechatinc.com/v3.3/customer/rtm/ws?license_id=10386012'
+            f'wss://api.livechatinc.com/v{stable_version}/customer/rtm/ws?{query_string}'
         )
         ws.send({'action': 'login', 'payload': {'token': 'Bearer xxx'}})
     assert str(exception.value) == 'Connection is already closed.'
@@ -45,7 +54,8 @@ def test_websocket_send_through_not_opened_pipe():
 def test_websocket_send_and_receive_message():
     ''' Test if websocket client sends and receives messages. '''
     ws = WebsocketClient(
-        url='wss://api.livechatinc.com/v3.3/customer/rtm/ws?license_id=10386012'
+        url=
+        f'wss://api.livechatinc.com/v{stable_version}/customer/rtm/ws?{query_string}'
     )
     ws.open()
     response = ws.send({'action': 'login', 'payload': {'token': 'Bearer xxx'}})
